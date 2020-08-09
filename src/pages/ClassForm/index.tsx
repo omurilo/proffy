d437eff1523102a59fadfbf6f3cd9e5196bf47ad
Murilo Henrique <oofleaoo@gmail.com>
2020-08-09 15:48:52 -0300

    feat: create class form to register new classes

--- src/pages/ClassForm/index.tsx
+++ src/pages/ClassForm/index.tsx
@@ -1,25 +1,247 @@
-import React from "react";
+import React, { useState, MouseEvent, useMemo } from "react";
+import { useHistory } from "react-router-dom";
 
 import Header from "../../components/Header";
+import Select, { Option } from "../../components/Select";
+import Textarea from "../../components/Textarea";
+import Input from "../../components/Input";
+
 import rocketIcon from "../../assets/images/icons/rocket.svg";
+import warningIcon from "../../assets/images/icons/warning.svg";
 
 import * as Styled from "./styles";
+import api from "../../services/api";
+import { ValueType } from "react-select/src/types";
 
 const ClassForm: React.FC = () => {
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [bio, setBio] = useState("");
+  const [subject, setSubject] = useState<Option>();
+  const [cost, setCost] = useState("");
+  const [week_day, setWeekDay] = useState<Option[]>([]);
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: "", from: "", to: "" },
+  ]);
+
+  const options: { [key: string]: Option } = useMemo(
+    () => ({
+      1: { value: "1", label: "Segunda" },
+      2: { value: "2", label: "Terça" },
+      3: { value: "3", label: "Quarta" },
+      4: { value: "4", label: "Quinta" },
+      5: { value: "5", label: "Sexta" },
+    }),
+    []
+  );
+
+  const history = useHistory();
+
+  const handleAddScheduleItem = (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    if (scheduleItems.length >= 5) return;
+
+    setScheduleItems([...scheduleItems, { week_day: "", from: "", to: "" }]);
+  };
+
+  const handleSetScheduleItemValue = (
+    index: number,
+    field: string,
+    value: string | Option
+  ) => {
+    if (field === "week_day") {
+      const updatedWeekDay = [...week_day];
+      updatedWeekDay[index] = value as Option;
+      setWeekDay(updatedWeekDay);
+    }
+    const updatedScheduleItems = scheduleItems.map(
+      (scheduleItem, scheduleItemIndex) => {
+        if (index === scheduleItemIndex) {
+          return {
+            ...scheduleItems[index],
+            [field]: (value as Option)?.value ?? value,
+          };
+        }
+
+        return scheduleItem;
+      }
+    );
+    setScheduleItems(updatedScheduleItems);
+  };
+
+  const handleCreateClass = (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    api.post("classes", {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject: subject?.value,
+      cost,
+      schedules: scheduleItems,
+    });
+
+    history.push("/");
+  };
+
   return (
     <Styled.Container>
       <Header
         title="Que incrível que você quer dar aulas."
         topBarTitle="Dar Aulas"
       >
-        <div style={{ gridArea: "children", display: "flex" }}>
-          <p>O primeiro passo, é preencher esse formulário de inscrição.</p>
-          <span>
+        <Styled.DescriptionContainer>
+          <Styled.Description>
+            O primeiro passo, é preencher esse formulário de inscrição.
+          </Styled.Description>
+          <Styled.DescriptionMotivation>
             <img src={rocketIcon} alt="Foguete verde" />
-            Preparare-se! vai ser o máximo.
-          </span>
-        </div>
+            <span>Preparare-se! vai ser o máximo.</span>
+          </Styled.DescriptionMotivation>
+        </Styled.DescriptionContainer>
       </Header>
+      <Styled.Main>
+        <Styled.Form>
+          <Styled.GroupForm>
+            <Styled.GroupFormLegend>Seus Dados</Styled.GroupFormLegend>
+            <Input
+              type="text"
+              name="name"
+              label="Nome completo"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              marginTop={"0rem"}
+            />
+            <Input
+              type="text"
+              name="avatar"
+              label="Link da sua foto"
+              value={avatar}
+              onChange={(e) => setAvatar(e.target.value)}
+              marginTop={"2.4rem"}
+            />
+            <Input
+              type="text"
+              name="whatsapp"
+              label="Whatsapp"
+              value={whatsapp}
+              onChange={(e) => setWhatsapp(e.target.value)}
+              marginTop={"2.4rem"}
+            />
+            <Textarea
+              name="bio"
+              value={bio}
+              label="Biografia"
+              marginTop={"2.4rem"}
+              onChange={(e) => setBio(e.target.value)}
+            />
+          </Styled.GroupForm>
+          <Styled.GroupForm>
+            <Styled.GroupFormLegend>Sobre a aula</Styled.GroupFormLegend>
+            <Select
+              name="subject"
+              label="Matéria"
+              placeholder="Selecione qual você quer ensinar"
+              value={
+                (subject?.value && subject) as ValueType<Option> | undefined
+              }
+              onChange={(value) => setSubject(value as Option)}
+              marginTop={"0rem"}
+              options={[
+                { value: "Artes", label: "Artes" },
+                { value: "Biologia", label: "Biologia" },
+                { value: "Ciências", label: "Ciências" },
+                { value: "Educação Física", label: "Educação Física" },
+                { value: "Física", label: "Física" },
+                { value: "Geografia", label: "Geografia" },
+                { value: "História", label: "História" },
+                { value: "Matemática", label: "Matemática" },
+                { value: "Português", label: "Português" },
+                { value: "Química", label: "Química" },
+              ]}
+            />
+            <Input
+              type="text"
+              name="cost"
+              label="Custa da sua aula por hora"
+              value={cost}
+              onChange={(e) => setCost(e.target.value)}
+              marginTop={"2.4rem"}
+            />
+          </Styled.GroupForm>
+          <Styled.GroupForm>
+            <Styled.GroupFormLegend>
+              Horarios disponíveis{""}
+              <Styled.Button to="" onClick={handleAddScheduleItem}>
+                + Novo horário
+              </Styled.Button>
+            </Styled.GroupFormLegend>
+            {scheduleItems.map((scheduleItem, index) => {
+              return (
+                <Styled.ScheduleItem
+                  key={String(Math.floor(Math.random() * Date.now()))}
+                >
+                  <Select
+                    name="week_day"
+                    label="Dia da semana"
+                    placeholder="Selecione"
+                    marginTop={"0rem"}
+                    value={
+                      (week_day[index]?.value && week_day[index]) as
+                        | ValueType<Option>
+                        | undefined
+                    }
+                    dimension={{ width: "100%" }}
+                    onChange={(selected) => {
+                      handleSetScheduleItemValue(
+                        index,
+                        "week_day",
+                        selected as Option
+                      );
+                    }}
+                    options={Object.values(options)}
+                  />
+                  <Input
+                    type="time"
+                    name="from"
+                    label="Das"
+                    value={scheduleItem.from}
+                    onChange={(e) =>
+                      handleSetScheduleItemValue(index, "from", e.target.value)
+                    }
+                    marginTop={"2.4rem"}
+                    dimension={{ width: "calc(50% - 0.4rem)" }}
+                  />
+                  <Input
+                    type="time"
+                    name="to"
+                    label="Até"
+                    value={scheduleItem.to}
+                    onChange={(e) =>
+                      handleSetScheduleItemValue(index, "to", e.target.value)
+                    }
+                    marginTop={"2.4rem"}
+                    dimension={{ width: "calc(50% - 0.4rem)" }}
+                  />
+                </Styled.ScheduleItem>
+              );
+            })}
+          </Styled.GroupForm>
+        </Styled.Form>
+        <Styled.Footer>
+          <Styled.AlertUser>
+            <img src={warningIcon} alt="Aviso importante!" />
+            <span>
+              Importante! <br />
+              Preencha todos os dados
+            </span>
+          </Styled.AlertUser>
+          <Styled.SaveClassButton to="" onClick={handleCreateClass}>
+            Salvar Cadastro
+          </Styled.SaveClassButton>
+        </Styled.Footer>
+      </Styled.Main>
     </Styled.Container>
   );
 };
--- src/pages/ClassForm/styles.ts
+++ src/pages/ClassForm/styles.ts
@@ -1,6 +1,157 @@
 import styled from "styled-components";
+import { Button as ButtonLink } from "../../components/ButtonLink";
+import { Container as Input } from "../../components/Input/styles";
+
+type StyledProps = {
+  key?: string;
+};
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
 `;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 2.4rem auto;
+`;
+
+export const Description = styled.p`
+  font: 1.4rem Poppins;
+  color: var(--color-text-in-primary);
+  line-height: 2.6rem;
+  max-width: 28.5rem;
+
+  @media (min-width: 720px) {
+    font-size: 1.6rem;
+  }
+`;
+
+export const DescriptionMotivation = styled.div`
+  display: none;
+  align-items: center;
+  justify-content: flex-end;
+
+  & span {
+    font: 1.2rem Poppins;
+    color: var(--color-text-in-primary);
+    max-width: 10.5rem;
+  }
+
+  & img {
+    margin-right: 2.4rem;
+  }
+
+  @media (min-width: 720px) {
+    display: flex;
+  }
+`;
+
+export const Main = styled.main`
+  width: 92.5%;
+  background-color: var(--color-box-base);
+  margin: -3.6rem auto 5.6rem;
+  border-radius: 0.8rem;
+
+  @media (min-width: 720px) {
+    max-width: 740px;
+  }
+`;
+
+export const Form = styled.form`
+  padding: 2.4rem 3.2rem;
+  margin: 0 auto;
+
+  @media (min-width: 720px) {
+    padding: 3.6rem 6.4rem;
+  }
+`;
+
+export const GroupForm = styled.fieldset`
+  border: 0;
+  margin-bottom: 3rem;
+`;
+
+export const GroupFormLegend = styled.legend`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem 0;
+  margin-bottom: 2.4rem;
+  width: 100%;
+  border-bottom: 1px solid var(--color-line-in-white);
+  color: var(--color-text-title);
+  font: 700 2.4rem Archivo;
+`;
+
+export const Button = styled(ButtonLink).attrs({
+  color: "primary",
+  background: "transparent",
+  hover: "transparent",
+  width: "15rem",
+  height: "3.6rem",
+  size: "1.6rem",
+})`
+  justify-content: flex-end;
+  :hover {
+    color: var(--color-primary-darker);
+  }
+`;
+
+export const ScheduleItem = styled.div<StyledProps>`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  & + & {
+    margin-top: 2.4rem;
+  }
+
+  @media (min-width: 720px) {
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr;
+    grid-gap: 1.6rem;
+
+    ${Input} {
+      margin-top: 0;
+    }
+  }
+`;
+
+export const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2.4rem;
+  background-color: var(--color-box-footer);
+  border-top: 1px solid var(--color-line-in-white);
+  border-bottom-left-radius: 0.8rem;
+  border-bottom-right-radius: 0.8rem;
+
+  @media (min-width: 720px) {
+    padding: 3.8rem 5.6rem;
+  }
+`;
+
+export const AlertUser = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & img {
+    margin-right: 1.6rem;
+  }
+
+  & span {
+    font: 500 1.2rem Poppins;
+    color: var(--color-text-complement);
+  }
+`;
+
+export const SaveClassButton = styled(ButtonLink).attrs({
+  height: "5.6rem",
+  width: "20rem",
+})``;
